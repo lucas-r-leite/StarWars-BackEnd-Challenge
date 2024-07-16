67ddd2d739ee18702c0abd9ee386c9d984c289c2
lucas <lucasgit.breeding934@passinbox.com>
2024-07-16 09:48:46 -0300

    Create spring-boot project

---
+++ starWarsPlanets/src/test/java/com/example/starWarsPlanets/StarWarsPlanetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.starWarsPlanets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsPlanetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
